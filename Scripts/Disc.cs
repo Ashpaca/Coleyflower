e6bd40e53c64ab892858812eb2f0a522fe9a9101
Ashpaca <ashley.ashley.harrison@gmail.com>
2024-11-14 21:06:01 -0800

    Started working on spicy

--- Scripts/Disc.cs
+++ Scripts/Disc.cs
@@ -5,6 +5,11 @@ public partial class Disc : RigidBody2D
 {
 	[Export]
 	public float radius { get; set; }
+	[Export]
+	public int health { get; set; }
+	[Export]
+	public int damage { get; set; }
+	// There will be more 
 
 	public override void _PhysicsProcess(double delta)
 	{	
