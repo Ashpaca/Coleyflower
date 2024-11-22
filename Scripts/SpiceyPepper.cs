e8393bd12f7919d99f5dccbdc6e6a7ab8f499835
Ashpaca <ashley.ashley.harrison@gmail.com>
2024-11-21 19:07:20 -0800

    Added the spicy flames

--- Scripts/SpiceyPepper.cs
+++ Scripts/SpiceyPepper.cs
@@ -3,13 +3,39 @@ using System;
 
 public partial class SpiceyPepper : Disc
 {
-	// Called when the node enters the scene tree for the first time.
+	const int MAX_FLAMES = 20;
+	PackedScene flameScene = GD.Load<PackedScene>("res://flame.tscn");
+	double timeScinceLastFlame = 0;
+	Node2D[] createdFlames;
+	int numberOfFlames = 0;
+
 	public override void _Ready()
 	{
+		base._Ready();
+		createdFlames = new Node2D[MAX_FLAMES];
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
+		base._PhysicsProcess(delta);
+		timeScinceLastFlame += delta;
+		if (timeScinceLastFlame >= .1 && numberOfFlames < MAX_FLAMES)
+		{
+			timeScinceLastFlame = 0;
+			Node2D flameInstance = flameScene.Instantiate<Node2D>();
+        	flameInstance.Position = GlobalPosition;
+			createdFlames[numberOfFlames] = flameInstance;
+			numberOfFlames++;
+        	GetTree().Root.AddChild(flameInstance);
+		}
+
+		if (numberOfFlames >= MAX_FLAMES)
+		{
+			for (int i = 0; i < MAX_FLAMES; i++)
+			{
+				createdFlames[i].QueueFree();
+			}
+			numberOfFlames = 0;
+		}
 	}
 }
